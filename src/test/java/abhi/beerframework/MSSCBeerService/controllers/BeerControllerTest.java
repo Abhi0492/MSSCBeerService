7b7f94d9238a62feb6cbb911f4dd43a3bceb2119
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-08-30 10:28:47 +0530

    commit towards my MSSC Beer Service Spring Boot Microservice 13

--- src/test/java/abhi/beerframework/MSSCBeerService/controllers/BeerControllerTest.java
+++ src/test/java/abhi/beerframework/MSSCBeerService/controllers/BeerControllerTest.java
@@ -1,6 +1,7 @@
 package abhi.beerframework.MSSCBeerService.controllers;
 
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import abhi.beerframework.MSSCBeerService.models.BeerDto;
+import abhi.beerframework.MSSCBeerService.models.BeerStyleEnum;
 
 
 
@@ -35,16 +37,15 @@ public class BeerControllerTest {
 		
 		
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-	         	.andExpect(MockMvcResultMatchers.status().isOk());
-		
-		
+	         	.andExpect(MockMvcResultMatchers.status().isOk());	
 		
 	}
 	
 	@Test
 	void saveNewBeer() throws Exception {
 		
-		BeerDto beerDto = BeerDto.builder().build();
+		//BeerDto beerDto = BeerDto.builder().build();
+		BeerDto beerDto = getValidBeer();
 		String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 		
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
@@ -57,7 +58,8 @@ public class BeerControllerTest {
 	@Test
 	void updateByBeerId() throws Exception {
 		
-		BeerDto beerDto = BeerDto.builder().build();
+		//BeerDto beerDto = BeerDto.builder().build();
+		BeerDto beerDto = getValidBeer();
 		String beerDtoString = objectMapper.writeValueAsString(beerDto);
 		
 		mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/")
@@ -65,9 +67,15 @@ public class BeerControllerTest {
 				.content(beerDtoString))
 				.andExpect(MockMvcResultMatchers.status().isNoContent());
 		
-		
-		
-		
+	}
+	
+	BeerDto getValidBeer() {
+		return BeerDto.builder()
+				.beerName("My Beer")
+				.beerStyle(BeerStyleEnum.AMERICAN_LAGER)
+				.price(new BigDecimal(160.34))
+				.upc(123123123123L)
+				.build();
 	}
 
 }
