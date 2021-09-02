578a7276d90f76d6a113b09db9fc3c051824e9c9
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-09-02 18:15:58 +0530

    commit towards my MSSC Beer Service Spring Boot Microservice 18

--- src/test/java/abhi/beerframework/MSSCBeerService/controllers/BeerControllerTest.java
+++ src/test/java/abhi/beerframework/MSSCBeerService/controllers/BeerControllerTest.java
@@ -5,11 +5,14 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -24,8 +27,8 @@ import abhi.beerframework.MSSCBeerService.models.BeerStyleEnum;
 import abhi.beerframework.MSSCBeerService.repositories.BeerRepository;
 
 
-
-
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages =  "abhi.beerframework.MSSCBeerService.mappers")
 public class BeerControllerTest {
